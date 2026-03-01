9afa0f13e0ed288dae1b67d12521bea2377485b4
Dor Raviv <dorraviv8@gmail.com>
2026-03-01 15:17:31 +0200

    adding secret and changing configuration

--- app/database.py
+++ app/database.py
@@ -1,12 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://user:password@db:5432/urlshortener"
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DB_HOST = os.getenv("DB_HOST", "postgres")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
+if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB]):
+    raise ValueError("Database environment variables are not set properly")
+
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
 )
 
 engine = create_engine(DATABASE_URL)
