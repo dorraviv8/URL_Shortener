f218eed1a920f8f8eabad8364b9adc3c87820bf4
Dor Raviv <dorraviv8@gmail.com>
2026-03-04 15:17:03 +0200

    editing database.py file

--- app/database.py
+++ app/database.py
@@ -9,16 +9,20 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 DB_HOST = os.getenv("DB_HOST", "postgres")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
-if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB]):
-    raise ValueError("Database environment variables are not set properly")
+DATABASE_URL = None
 
-DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
-)
+if POSTGRES_USER and POSTGRES_PASSWORD and POSTGRES_DB:
+    DATABASE_URL = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+    )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL) if DATABASE_URL else None
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+) if engine else None
 
 Base = declarative_base()
